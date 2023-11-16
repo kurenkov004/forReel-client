640d8e47708f3a6adf37ac637e6622ee5ef8db19
kurenkov004 <mary.kurenkov@gmail.com>
2023-11-15 21:29:36 -0500

    linked MainView component

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,21 +1,20 @@
+//This file only has one job: to bootstrap React code and nothing else
+
 import { createRoot } from 'react-dom/client';
+import { MainView } from './components/main-view/main-view';
 
 // Import statement, specifies that './index.scss' needs to be bundled
 import "../styles/index.scss";
 
 //Main component
 const ForReelApplication = () => {
-  return (
-    <div className='for-reel'>
-      <div>Good morning</div>
-    </div>
-  );
+  return <MainView />
 };
 
 //Finds the root of the app
 const container = document.querySelector("#root");
 const root = createRoot(container);
 
-//Tells React to render the app in the roor DOM element
+//Tells React to render the app in the root DOM element
 root.render(<ForReelApplication />);
 
