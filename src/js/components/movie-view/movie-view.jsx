f0211ddb499cd6e5d389ad1a551a807decbcca17
kurenkov004 <mary.kurenkov@gmail.com>
2023-11-29 14:40:22 -0500

    updated PropTypes & references to match API format

--- src/js/components/movie-view/movie-view.jsx
+++ src/js/components/movie-view/movie-view.jsx
@@ -1,26 +1,39 @@
+import PropTypes from "prop-types"; //imports PropTypes Library
+
 export const MovieView = ({ expandedMovie, onBackClick }) => {
   return ( //remember, there can only be ONE root element in a component
     <div>
       <div>
-        <img src="{expandedMovie.image}" />
+        <img src={expandedMovie.ImagePath} />
       </div>
       <div>
         <span>Title: </span>
-        <span>{expandedMovie.title}</span>
+        <span>{expandedMovie.Title}</span>
       </div>
       <div>
         <span>Description: </span>
-        <span>{expandedMovie.description}</span>
+        <span>{expandedMovie.Description}</span>
       </div>
       <div>
         <span>Genre: </span>
-        <span>{expandedMovie.genre}</span>
+        <span>{expandedMovie.Genre.Name}</span>
       </div>
       <div>
         <span>Director: </span>
-        <span>{expandedMovie.director}</span>
+        <span>{expandedMovie.Director.Name}</span>
       </div>
       <button onClick={onBackClick}>Back</button>
     </div>
   );
 };
+
+//this defines all the prop constraints for MovieView
+MovieView.propTypes = {
+  expandedMovie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    Genre: PropTypes.string,
+    Director: PropTypes.string,
+    Image: PropTypes.string
+  }).isRequired
+}
