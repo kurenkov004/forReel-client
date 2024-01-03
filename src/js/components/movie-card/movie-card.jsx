25c22da9f870a2f3ae6b9f1fa93f227f22960e8d
kurenkov004 <mary.kurenkov@gmail.com>
2024-01-02 20:56:16 -0500

    added basic Card styling

--- src/js/components/movie-card/movie-card.jsx
+++ src/js/components/movie-card/movie-card.jsx
@@ -1,14 +1,25 @@
 import PropTypes from "prop-types"; //imports PropTypes Library
+import { Button, Card} from "react-bootstrap";
 
 export const MovieCard = ({ movieData, onMovieClick }) => { //destructured props argument that lets BookCard access data from MainView file
   return (
-    <div
-      onClick={() => {
-        onMovieClick(movieData)
-      }}
-    >
-      {movieData.Title}
-    </div>
+    <Card>
+      <Card.Img variant="top" src={movieData.ImagePath} />
+      <Card.Body>
+        <Card.Title>{movieData.Title}</Card.Title>
+        <Card.Text>{movieData.Director.Name}</Card.Text>
+        <Button onClick={() => onMovieClick(movieData)} variant="link">
+          Open
+        </Button>
+      </Card.Body>
+    </Card>
+    // <div
+    //   onClick={() => {
+    //     onMovieClick(movieData)
+    //   }}
+    // >
+    //   {movieData.Title}
+    // </div>
   );
 };
 
