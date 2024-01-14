305347069ad6f7e38341e81f713bbdcb8640d2cc
kurenkov004 <mary.kurenkov@gmail.com>
2024-01-13 23:33:30 -0500

    added Link and removed onClick

--- src/js/components/movie-card/movie-card.jsx
+++ src/js/components/movie-card/movie-card.jsx
@@ -1,18 +1,19 @@
 import PropTypes from "prop-types"; //imports PropTypes Library
 import { Button, Card} from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 import "./movie-card.scss";
 
-export const MovieCard = ({ movieData, onMovieClick }) => { //destructured props argument that lets BookCard access data from MainView file
+export const MovieCard = ({ movieData }) => { //destructured props argument that lets BookCard access data from MainView file
   return (
     <Card className="h-100">
       <Card.Img variant="top" src={movieData.ImagePath} />
       <Card.Body>
         <Card.Title>{movieData.Title}</Card.Title>
         <Card.Text>{movieData.Director.Name}</Card.Text>
-        <Button onClick={() => onMovieClick(movieData)} variant="link">
-          Open
-        </Button>
+        <Link to={`/movies/${encodeURIComponent(movie.id)}`} >
+          <Button variant="link">Open</Button>
+        </Link>
       </Card.Body>
     </Card>
   );
@@ -23,5 +24,5 @@ MovieCard.propTypes = {
   movieData: PropTypes.shape({
     Title: PropTypes.string
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
+  // onMovieClick: PropTypes.func.isRequired
 }
